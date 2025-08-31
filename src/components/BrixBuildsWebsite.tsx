b6d858f390bfcbdb031199ee8728b132eb2d50e7
defidrum <your.email@example.com>
2025-08-30 19:12:24 -0500

    Brix builds site

--- src/components/BrixBuildsWebsite.tsx
+++ src/components/BrixBuildsWebsite.tsx
@@ -293,18 +293,7 @@ const BrixBuildsWebsite = () => {
           {/* Scroll Indicator */}
         </div>
 
-        {/* Hero Image Indicators */}
-        <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 flex space-x-2">
-          {heroImages.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrentImageIndex(index)}
-              className={`w-3 h-3 rounded-full transition-all duration-300 ${
-                index === currentImageIndex ? 'bg-white scale-125' : 'bg-white/50'
-              }`}
-            />
-          ))}
-        </div>
+        
       </section>
 
       {/* Stats Section */}
